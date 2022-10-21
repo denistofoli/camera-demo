abef667c90bb3199c07f3cfc1e55d11bd81e6e12
Denis Tofoli <denis_tofoli@msn.com>
2022-10-21 12:47:54 -0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { WebcamModule } from 'ngx-webcam';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, WebcamModule],
   providers: [],
   bootstrap: [AppComponent]
 })
