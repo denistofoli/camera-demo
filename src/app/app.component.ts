fbf31e2bb3f2ee8dbc44cb9d952f40da3c720f46
Denis Tofoli <denis_tofoli@msn.com>
2022-10-23 15:07:11 -0300

    refactor: clean

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
+import { WebcamImage } from 'ngx-webcam';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   private trigger: Subject<any> = new Subject();
   public webcamImage!: WebcamImage;
-  private nextWebcam: Subject<any> = new Subject();
   sysImage = '';
-  ngOnInit() {}
+
   public getSnapshot(): void {
     this.trigger.next(void 0);
   }
+
   public captureImg(webcamImage: WebcamImage): void {
     this.webcamImage = webcamImage;
     this.sysImage = webcamImage!.imageAsDataUrl;
     console.info('got webcam image', this.sysImage);
   }
+
   public get invokeObservable(): Observable<any> {
     return this.trigger.asObservable();
-  }
-  public get nextWebcamObservable(): Observable<any> {
-    return this.nextWebcam.asObservable();
   }
 }
